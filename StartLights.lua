acd583d7d1e6437df1f07641cb120f7058d04534
NaikeG <ema.grasso.96@gmail.com>
2026-07-10 22:25:56 -0300

    Test

--- StartLights.lua
+++ StartLights.lua
@@ -10,6 +10,7 @@ panelPreviewEvent = ac.OnlineEvent({
     key = ac.StructItem.key("Panel Preview Mode"),
     selectedId = ac.StructItem.float()
 }, function(sender, message)
+    if sender:driverName() ~= car:driverName() then return end
     editingPanelId = message.selectedId
 end,
 ac.SharedNamespace.ServerScript)
